6a4bca1011ee62aa4a895adfb325a640a46551e0
Anton Mamontov <anton_mamont@bk.ru>
2020-05-13 09:16:57 +0300

    Updates

---
+++ precache-manifest.5e04a19b1b2e67ad98a3241702d182ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09597a3049dcb2586cd26b3e3bd0bf8",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "4c909cf65ee1a6106364",
+    "url": "/page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef570cb236774a730f18",
+    "url": "/page/static/js/2.c3515144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/page/static/js/2.c3515144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c909cf65ee1a6106364",
+    "url": "/page/static/js/main.3da946bd.chunk.js"
+  },
+  {
+    "revision": "c1c512b5d44196cf69a3",
+    "url": "/page/static/js/runtime-main.618c66b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/page/static/media/logo.5d5d9eef.svg"
+  }
+]);
